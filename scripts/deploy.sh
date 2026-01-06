d5cc88901b7bf729c13e581021e77468062da3b7
hello-world <hello-world@gmail.com>
2026-01-06 22:19:37 +0530

    conf files updated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -e
 
-aws ecr get-login-password --region us-east-1 |
+aws ecr get-login-password --region us-east-1 | \
 docker login --username AWS --password-stdin 991940085316.dkr.ecr.us-east-1.amazonaws.com
 
-echo "Stopping the container if already exists"
+docker network create app-network || true
+
 docker stop nginx || true
 docker rm nginx || true
 
-echo "Pulling latest image from ECR"
 docker pull 991940085316.dkr.ecr.us-east-1.amazonaws.com/nginx:latest
 
-echo "Starting nginx container"
 docker run -d \
   --name nginx \
   --network app-network \
